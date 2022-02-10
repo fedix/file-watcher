fa1471d8af22947ebacdecd04b160a9dfa042e13
fedosique <d.fedor97@gmail.com>
2022-02-10 19:15:16 +0300

    Refactor Synchronizer

--- src/main/scala/fedosique/file/watcher/lib/Synchronizer.scala
+++ src/main/scala/fedosique/file/watcher/lib/Synchronizer.scala
@@ -16,37 +16,33 @@ trait Synchronizer[F[_]] {
 object Synchronizer {
   class FileSynchronizer[F[_]: Files: Concurrent: Logger](override val source: Path, replica: Path, watcher: Watcher[F])
       extends Synchronizer[F] {
-    import cats.syntax.functor._
     import cats.syntax.flatMap._
     import cats.syntax.applicative._
 
     private def copy(path: Path) = {
-      def copyFile(path: Path) =
-        info"replicating $path to ${replica / source.relativize(path)}" >>
-          Files[F].copy(path, replica / source.relativize(path), CopyFlags(CopyFlag.ReplaceExisting))
+      val target = replica / source.relativize(path)
+      val copyFile =
+        info"replicating $path to $target" >>
+          Files[F].copy(path, target, CopyFlags(CopyFlag.ReplaceExisting))
 
-      def copyDir(dir: Path) =
+      val copyDir =
         Files[F]
-          .exists(replica / source.relativize(dir))
+          .exists(target)
           .ifM(
             ().pure,
-            info"creating${replica / source.relativize(dir)}" >>
-              Files[F].createDirectory(replica / source.relativize(dir))
+            info"creating $target" >> Files[F].createDirectory(target)
           )
-          .flatTap(_ => info"replicating recursively $dir")
-          .flatMap(_ => synchronize(dir).compile.drain)
+          .flatTap(_ => info"replicating recursively $path")
+          .flatMap(_ => synchronize(path).compile.drain)
 
-      Files[F].isDirectory(path).ifM(copyDir(path), copyFile(path))
+      Files[F].isDirectory(path).ifM(copyDir, copyFile)
     }
 
     private def delete(path: Path) = {
-      def deleteFile(path: Path) =
-        info"deleting $path" >> Files[F].delete(path)
+      val deleteFile = info"deleting $path" >> Files[F].delete(path)
+      val deleteDir  = info"deleting recursively $path" >> Files[F].deleteRecursively(path)
 
-      def deleteDir(path: Path) =
-        info"deleting recursively $path" >> Files[F].deleteRecursively(path)
-
-      Files[F].isDirectory(path).ifM(deleteDir(path), deleteFile(path))
+      Files[F].isDirectory(path).ifM(deleteDir, deleteFile)
     }
 
     override def synchronize(dir: Path = source): Stream[F, Unit] =
@@ -54,11 +50,10 @@ object Synchronizer {
         Stream
           .eval(watcher.filesToUpdate(dir))
           .evalTap(wr => info"$wr")
-          .evalTap(wr =>
-            (Stream.emits[F, Path](wr.toCopy).mapAsync(4)(copy) ++
-              Stream.emits[F, Path](wr.toDelete).mapAsync(4)(delete)).compile.drain
+          .flatMap(wr =>
+            Stream.emits[F, Path](wr.toCopy).mapAsync(4)(copy) ++
+              Stream.emits[F, Path](wr.toDelete).mapAsync(4)(delete)
           )
-          .void
   }
 
   def make[F[_]: Files: Async](source: Path, replica: Path, watcher: Watcher[F]): F[FileSynchronizer[F]] = {
